3eb565cccb5b79eeee06c5af2fb354f36c1227fd
OM Miles <miles@ommiles.com>
2021-05-19 10:55:36 -0700

    javascript algo complete

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,11 +1,23 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  // Given an Array, find the first duplicate value that occurs. If there are no duplicates, return -1.
+  // loop through array and compare current and next values of array
+  // if any values in array are equal to other values, return -1
+  // if not, return VALUE of FIRST element(s)
+  // must be sequential...
+  
+  for( let i=0; i < arr.length ; i++) {
+    if(arr[i] === arr[i+1]) {
+      return arr[i]
+    } 
+  }
+
+  return -1
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+  console.log("Expecting: 7");
+  console.log("=>", findFirstDuplicate([7, 7, 2, 1, 3, 7, 3, 2]));
 
   console.log("");
 
